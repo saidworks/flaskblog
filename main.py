976c828c5d230a57e6d48b980c49484d0737b023
Said ZITOUNI <zitouni.sd@gmail.com>
2022-05-05 22:00:32 -0400

    add create page form and POST method done need to work in displaying all results

--- main.py
+++ main.py
@@ -19,24 +19,13 @@ def create_table():
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'GET':
-        return render_template('createpage.html')
+        return render_template('create.html')
 
-
-# initialize and setup app
-app = Flask(__name__, template_folder="templates")
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/Blog.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-
-
-# create table
-@app.before_first_request
-def create_table():
-    db.create_all()
-
-
-@app.route('/create', methods=['GET', 'POST'])
-def create():
-    if request.method == 'GET':
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        date = request.form['date']
+        post = PostModel(title, content, date)
+        db.session.add(post)
+        db.session.commit()
         return render_template('index.html')
--- models.py
+++ models.py
@@ -1,25 +1,35 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+# cast string to datetime
+class MyDateTime(db.TypeDecorator):
+    impl = db.DateTime
+
+    def process_bind_param(self, value, dialect):
+        if type(value) is str:
+            return datetime.datetime.strptime(value, '%Y-%m-%d')
+        return value
+
+
 # define the post model
-
-
 class PostModel(db.Model):
     __tablename__ = "post"
 
     post_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String())
     content = db.Column(db.Text(), nullable=True)
-    date = db.Column(db.DateTime(), nullable=True)
-    author = db.Column(db.String())
+    date = db.Column(MyDateTime, nullable=True)
 
-    def __init__(self, post_id, title, content, date, author):
-        self.post_id = post_id
+    def __init__(self, title, content, date):
+        count = 1
+        self.post_id = count
         self.title = title
         self.content = content
         self.date = date
-        self.author = author
+        count += 1
 
     def __repr__(self):
-        return f"{self.title}: {self.content} writter by {self.author} on {self.date}"
+        return f"{self.title}: {self.content} written by Said Zitouni on {self.date}"
